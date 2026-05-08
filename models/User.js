acbc65a471d2c5802fc98f89ec3ccd697f53f53e
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-05-08 21:39:00 +0200

    create user model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+// Import mongoose
+const mongoose = require('mongoose');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+// Export the User model
+module.exports = User;
